9ef76f7e9d1ca990c352022a4747b6a160dd5683
Gromych <gromych@yandex-team.ru>
2017-04-22 15:21:44 -0700

    Adds react to a boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const htmlTemplate = new HtmlWebpackPlugin({
+	template: './client/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
 module.exports = {
 	entry: './client/index.js',
 	output: {
@@ -17,6 +23,6 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new HtmlWebpackPlugin()
+		htmlTemplate
 	]
 };
