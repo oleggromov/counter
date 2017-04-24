7135f1a59878965b01d652272c4bfe5f807c084b
Gromych <gromych@yandex-team.ru>
2017-04-23 23:45:08 -0700

    Lints configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const htmlTemplate = new HtmlWebpackPlugin({
-	template: './client/index.html',
-	filename: 'index.html',
-	inject: 'body'
-});
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 
 module.exports = {
-	entry: './client/index.js',
-	output: {
-		path: path.resolve('dist'),
-		filename: 'app.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader',
-				exclude: '/node_modules/'
-			},
-			{
-				test: /\.css$/,
-				loader: ExtractTextPlugin.extract('css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'),
-			}
-		]
-	},
-	plugins: [
-		htmlTemplate,
-		new ExtractTextPlugin('styles.css')
-	]
-};
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+      }
+    ]
+  },
+  plugins: [
+    htmlTemplate,
+    new ExtractTextPlugin('styles.css')
+  ]
+}
