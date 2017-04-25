0bfec62cbf16a6296b7cdff34bbf3d7abd20b9e4
Gromych <gromych@yandex-team.ru>
2017-04-24 18:22:00 -0700

    Adds dummy colors changing for different layouts

--- client/components/Layout/Layout.jsx
+++ client/components/Layout/Layout.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import MediaDetector from '../../media-detector.js'
 import styles from './layout.css'
 
+const layouts = {
+  // iphone 7+ screen width is 414px
+  '(max-device-width: 414px) and (orientation: portrait)': 'portrait',
+  // iphone 7+ screen width in landscape orientation is 736px
+  '(max-device-width: 736px) and (orientation: landscape)': 'landscape',
+  '(min-device-width: 737px)': 'default'
+}
+
+function getLayoutTypeClass (type) {
+  return styles[`layout_${type}`]
+}
+
 export default class Layout extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      type: 'default'
+    }
+  }
+
+  componentDidMount () {
+    this.mediaDetector = new MediaDetector(layouts, type => {
+      this.setState({ type })
+    })
+  }
+
+  componentWillUnmount () {
+    this.mediaDetector.destroy()
+  }
+
   render () {
+    const layoutClass = getLayoutTypeClass(this.state.type)
+
     return (
-      <div className={styles.layout}>
+      <div className={`${styles.layout} ${layoutClass}`}>
         <div className={styles.header}>
           <div className={styles.logo}>Counter <sup>0.1</sup></div>
         </div>
