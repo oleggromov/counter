6fb3e146f2bb3ba234036250b4663d334de25bae
Gromych <gromych@yandex-team.ru>
2017-04-25 23:22:53 -0700

    Changes SpentListItem back to simple function

--- client/components/SpentListItem/SpentListItem.jsx
+++ client/components/SpentListItem/SpentListItem.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styles from './spent-list-item.css'
 
 const currency = '$'
 
-export default class SpentListItem extends Component {
-  render () {
-    const item = this.props.item
+export default function SpentListItem (props) {
+  const item = props.item
 
-    return (
-      <div className={`${styles.row} ${styles[this.props.mediaType]}`}>
-        <div className={styles.currency}>
-          {currency}
-        </div>
-        <div className={styles.amount}>
-          {item.amount.toFixed(2)}
-        </div>
-        <div className={styles.name}>
-          {item.type}
-        </div>
-        <div className={styles.delete}>
-          <span onClick={this.props.onDelete}>Delete</span>
-        </div>
+  return (
+    <div className={`${styles.row} ${styles[props.mediaType]}`}>
+      <div className={styles.currency}>
+        {currency}
       </div>
-    )
-  }
+      <div className={styles.amount}>
+        {item.amount.toFixed(2)}
+      </div>
+      <div className={styles.name}>
+        {item.type}
+      </div>
+      <div className={styles.delete}>
+        <span onClick={props.onDelete}>Delete</span>
+      </div>
+    </div>
+  )
 }
