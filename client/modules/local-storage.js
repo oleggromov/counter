8636b29e50ff89a3c65327227e4dbc40b852ce64
Gromych <gromych@yandex-team.ru>
2017-05-01 12:24:56 -0700

    Fixes initial data for an empty localStorage

--- client/modules/local-storage.js
+++ client/modules/local-storage.js
@@ -1,10 +1,10 @@
 const storage = window.localStorage
 const localStorageKey = 'data'
-const noData = { lists: [] }
+const emptyDaya = { lists: { nextId: 0, items: [] } }
 
 const get = () => {
   const serialized = storage.getItem(localStorageKey)
-  return JSON.parse(serialized) || noData
+  return JSON.parse(serialized) || emptyDaya
 }
 
 const set = (data) => {
