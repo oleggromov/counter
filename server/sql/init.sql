10c11f1d9cf53c89d8e68ca907fc575398eb2557
Gromych <gromych@yandex-team.ru>
2017-05-04 22:56:10 -0700

    Updates init.sql

--- server/sql/init.sql
+++ server/sql/init.sql
@@ -1,6 +1,7 @@
 # as root
 # CREATE USER 'counter'@'localhost' IDENTIFIED BY '';
-# GRANT ALL PRIVILEGES ON counter_0_1 . * TO 'counter'@'localhost';
+# GRANT ALL PRIVILEGES ON `counter_0_1` . * TO 'counter'@'localhost';
+# FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS `counter_0_1`
 	DEFAULT CHARSET `utf8`
