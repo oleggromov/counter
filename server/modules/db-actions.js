626d120a6c19d904520523623e5021b6b3bb735b
Gromych <gromych@yandex-team.ru>
2017-05-05 14:29:15 -0700

    Changes SQL query to a correct one which returns not only array of Lists but also items count within and the last item date

--- server/modules/db-actions.js
+++ server/modules/db-actions.js
@@ -1,11 +1,18 @@
-const sql = {
-  GET_LISTS: 'SELECT * FROM `lists`',
+let sql = {
   GET_ITEMS: 'SELECT * FROM `items` WHERE `listId` = ? ORDER BY `date` DESC',
   CREATE_LIST: 'INSERT INTO `lists` (`name`) VALUES (?)',
   CREATE_ITEM: 'INSERT INTO `items` (`listId`, `name`, `value`, `date`) VALUES (?, ?, ?, ?)',
   DELETE_LIST: 'DELETE FROM `lists` WHERE `id` = ?',
   DELETE_ITEM: 'DELETE FROM `items` WHERE `listId` = ? AND `id` = ?'
 }
+
+sql.GET_LISTS = `SELECT lists.id, lists.name,
+    COUNT(items.id) AS itemsCount,
+    MAX(items.date) AS lastDate
+  FROM lists
+  LEFT JOIN items
+    ON lists.id = items.listId
+  GROUP BY lists.id`
 
 const defaultResolver = (resolve, result) => {
   resolve(result)
