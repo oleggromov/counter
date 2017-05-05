da78cd631b8209c29cb3ec6a91ab4fd52a1e77d2
Gromych <gromych@yandex-team.ru>
2017-05-05 10:31:48 -0700

    Fixes client-side routing by serving index.html for any of URLs

--- server/app.js
+++ server/app.js
@@ -4,11 +4,18 @@ const resolveToRoot = require('./modules/resolve-to-root')
 const apiRouter = require('./api/router')
 
 const app = express()
-const staticPath = resolveToRoot(config.staticPath)
 
-app.use('/', express.static(staticPath))
-console.log(`Serving static from ${staticPath}`)
-
+// API requests go first
 app.use(`/api/${config.apiVersionUrl}/`, apiRouter)
+
+// Everything that is found in public/ directory is served as static
+app.use('/', express.static(resolveToRoot(config.staticPath), {
+  fallthrough: true
+}))
+
+// And if nothing was found, send index.html by default
+app.use(/.*/, (req, res) => {
+  res.sendFile(resolveToRoot('public/index.html'))
+})
 
 app.listen(config.port, () => console.log(`Server is running on ${config.port}`))
