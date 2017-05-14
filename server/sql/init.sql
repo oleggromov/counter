2db56add89806229332f310324465c67e9f963b2
Oleg Gromov <mail@oleggromov.com>
2017-05-14 14:33:32 -0700

    Updates queries to represent new userId fields

--- server/sql/init.sql
+++ server/sql/init.sql
@@ -11,25 +11,31 @@ USE `counter_0_1`;
 
 DROP TABLE `items`;
 DROP TABLE `lists`;
-
-CREATE TABLE IF NOT EXISTS `lists` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `items` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`listId` INT NOT NULL,
-	`name` TEXT NOT NULL,
-	`date` DATETIME NOT NULL,
-	`value` DECIMAL(10,2) NOT NULL,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`listId`) REFERENCES `lists`(`id`)
-);
+DROP TABLE `users`;
 
 CREATE TABLE IF NOT EXISTS `users` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`facebookId` VARCHAR(128) NOT NULL UNIQUE,
 	PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `lists` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`userId` INT NOT NULL,
+	`name` TEXT NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`userId`) REFERENCES `users`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `items` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`listId` INT NOT NULL,
+	`userId` INT NOT NULL,
+	`name` TEXT NOT NULL,
+	`date` DATETIME NOT NULL,
+	`value` DECIMAL(10,2) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`listId`) REFERENCES `lists`(`id`),
+	FOREIGN KEY (`userId`) REFERENCES `users`(`id`)
+);
+
