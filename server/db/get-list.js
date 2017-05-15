d9f3a6c406b595eff9aa4231c1a75326e9cf853d
Oleg Gromov <mail@oleggromov.com>
2017-05-14 23:23:38 -0700

    Moves list existance check into a separate then throwing an error if something is wrong

--- server/db/get-list.js
+++ server/db/get-list.js
@@ -5,37 +5,41 @@ const SQL = require('./queries')
 const getList = (defaultError, {userId, listId}, {excludeItems}) => {
   // return hasPermission({ userId, listId })
 
+  const checkIfListExists = data => {
+    if (!data.length) {
+      throw new APIResponse({
+        status: APIResponse.CODES.NOT_FOUND,
+        error: {
+          message: defaultError
+        }
+      })
+    }
+    return data
+  }
+
   const promises = [
-    makeQuery(SQL.getLists({ singleList: true }), [listId]),
+    makeQuery(SQL.getLists({ singleList: true }), [listId]).then(checkIfListExists),
     makeQuery(SQL.listItems, [listId])
   ]
 
   return Promise.all(promises)
-    .then(results => {
-      const listInfo = results[0][0]
-      const items = { items: results[1] }
-
-      if (!listInfo) {
-        return new APIResponse({
-          status: APIResponse.CODES.NOT_FOUND,
-          error: {
-            message: defaultError
-          }
-        })
+    .then(results => new APIResponse({
+      status: APIResponse.CODES.OK,
+      data: Object.assign({}, results[0][0], { items: results[1] })
+    }))
+    .catch(err => {
+      if (err instanceof APIResponse) {
+        return err
       }
 
       return new APIResponse({
-        status: APIResponse.CODES.OK,
-        data: Object.assign({}, listInfo, items)
+        status: APIResponse.CODES.SERVER_ERROR,
+        error: {
+          data: err,
+          message: defaultError
+        }
       })
     })
-    .catch(err => new APIResponse({
-      status: APIResponse.CODES.SERVER_ERROR,
-      error: {
-        data: err,
-        message: defaultError
-      }
-    }))
 }
 
 module.exports = getList
