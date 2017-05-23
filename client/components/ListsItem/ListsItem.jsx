29ac315fa46211c94acc60eb259b36760496908a
Oleg Gromov <mail@oleggromov.com>
2017-05-23 11:32:12 -0700

    Adds isLoading state to ListsItem

--- client/components/ListsItem/ListsItem.jsx
+++ client/components/ListsItem/ListsItem.jsx
@@ -2,54 +2,92 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import DateFormatted from '../DateFormatted/DateFormatted.jsx'
 import styles from './lists-item.css'
+import Loading from '../Loading/Loading.jsx'
 
 const strings = {
   pcs: 'pcs',
   delete: 'Delete'
 }
 
-const ListsItem = props => {
-  const renderDate = () => {
-    if (props.date) {
-      return (
-        <div className={styles.date}>
-          <DateFormatted contracted date={props.date} />
-        </div>
-      )
-    }
+const renderDate = (date, isLoading) => {
+  if (date && !isLoading) {
+    return (
+      <div className={styles.date}>
+        <DateFormatted contracted date={date} />
+      </div>
+    )
+  }
+}
+
+const getClasses = (isLoading, isReadyToDelete) => {
+  let classes = styles.listsItem
+
+  if (isLoading) {
+    classes = `${classes} ${styles.isLoading}`
+  } else if (isReadyToDelete) {
+    classes = `${classes} ${styles.isReadyToDelete}`
   }
 
+  return classes
+}
+
+const renderLoading = () => {
+  return (
+    <div className={styles.loading}>
+      <Loading />
+    </div>
+  )
+}
+
+const renderDeleteButton = (onDelete) => {
   const notifyDeletion = (e) => {
     e.preventDefault()
-    props.onDelete()
-  }
-
-  let classes = styles.listsItem
-  if (props.showDelete) {
-    classes = `${classes} ${styles.readyToDelete}`
+    onDelete()
   }
 
   return (
-    <Link
-      className={classes}
-      to={props.link}>
+    <div className={styles.delete} onClick={notifyDeletion}>
+      {strings.delete}
+    </div>
+  )
+}
+
+const renderCount = (count, isLoading) => {
+  if (!isLoading) {
+    return (
+      <div className={styles.count}>
+        {count} {strings.pcs}
+      </div>
+    )
+  }
+}
+
+const ListsItem = ({ children, link, date, count, isLoading, isReadyToDelete, onDelete }) => {
+  const classes = getClasses(isLoading, isReadyToDelete)
+
+  let control
+  let onClick
+
+  if (isLoading) {
+    control = renderLoading()
+    onClick = (e) => e.preventDefault()
+  } else {
+    control = renderDeleteButton(onDelete)
+  }
+
+  return (
+    <Link className={classes} to={link} onClick={onClick}>
       <div className={styles.link}>
         <span className={styles.linkText}>
-          {props.children}
+          {children}
         </span>
       </div>
 
-      { renderDate() }
+      { renderDate(date, isLoading) }
 
-      <div className={styles.count}>
-        {props.count} {strings.pcs}
-      </div>
+      { renderCount(count, isLoading) }
 
-      <div
-        className={styles.delete}
-        onClick={notifyDeletion}>
-        {strings.delete}
-      </div>
+      {control}
     </Link>
   )
 }
