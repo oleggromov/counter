7db8ee75aefc6194d30f36a940993bd175050583
Oleg Gromov <mail@oleggromov.com>
2017-05-27 23:32:48 -0700

    Adds error handling logic to App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import detectMedia from '../modules/detect-media'
 import api from '../modules/api'
+import cloneAndMutate from '../modules/clone-and-mutate'
 import Layout from './Layout/Layout.jsx'
 import ScreenMain from './ScreenMain/ScreenMain.jsx'
 import ScreenList from './ScreenList/ScreenList.jsx'
 import ScreenLogin from './ScreenLogin/ScreenLogin.jsx'
 import ScreenSettings from './ScreenSettings/ScreenSettings.jsx'
+import Message from './Message/Message.jsx'
 
 const afterDeletedUrl = '/'
 
@@ -14,11 +16,6 @@ const mediaType = detectMedia({
   '(max-device-width: 799px)': 'mobile',
   '(min-device-width: 800px)': 'desktop'
 })
-
-const handleError = (err) => {
-  window.alert('Error! Open dev console for details')
-  console.error(err)
-}
 
 const getComponentRender = (Component, commonProps) => {
   return (props) => {
@@ -34,7 +31,9 @@ export default class App extends Component {
     super(props)
 
     this.state = {
-      loginData: null
+      loginData: null,
+      errors: [],
+      currentError: null
     }
   }
 
@@ -60,6 +59,43 @@ export default class App extends Component {
       })
   }
 
+  dispatchError (originalError) {
+    const { message, text } = originalError.error
+    const err = {
+      title: message,
+      text
+    }
+
+    this.setState(cloneAndMutate(state => {
+      if (!state.currentError) {
+        state.currentError = err
+      } else {
+        state.errors.push(err)
+      }
+    }))
+  }
+
+  showNextError () {
+    if (this.state.errors.length) {
+      this.setState(cloneAndMutate(state => {
+        state.currentError = state.errors.shift()
+      }))
+    } else {
+      this.setState({ currentError: null })
+    }
+  }
+
+  renderMessage () {
+    const error = this.state.currentError || { title: null, text: null }
+
+    return (
+      <Message
+        title={error.title}
+        text={error.text}
+        onClose={this.showNextError.bind(this)} />
+    )
+  }
+
   render () {
     let classes = 'app'
     if (mediaType !== 'mobile') {
@@ -67,7 +103,7 @@ export default class App extends Component {
     }
 
     const screenProps = {
-      onError: handleError
+      onError: this.dispatchError.bind(this)
     }
 
     const login = getComponentRender(ScreenLogin, screenProps)
@@ -77,15 +113,19 @@ export default class App extends Component {
       onDeleteUser: this.deleteUser.bind(this)
     }))
 
+    const hasError = Boolean(this.state.currentError)
+
     return (
       <Router>
         <div className={classes}>
-          <Layout loginData={this.state.loginData}>
+          <Layout loginData={this.state.loginData} blur={hasError}>
             <Route path='/auth/login' render={login} />
             <Route exact path='/' render={main} />
             <Route path='/lists/:id' render={list} />
             <Route path='/settings' render={settings} />
           </Layout>
+
+          { this.renderMessage() }
         </div>
       </Router>
     )
