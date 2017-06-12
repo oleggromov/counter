7c74eca52fe3735ccd9a71408603178df9367e3e
Oleg Gromov <mail@oleggromov.com>
2017-06-11 21:00:58 -0700

    Changes date column to bigint and updates mocks to store JS date values

--- server/sql/init.sql
+++ server/sql/init.sql
@@ -10,38 +10,4 @@ ALTER DATABASE `counter_0_1`
 
 USE `counter_0_1`;
 
-DROP TABLE IF EXISTS `items`;
-DROP TABLE IF EXISTS `permissions`;
-DROP TABLE IF EXISTS `lists`;
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE IF NOT EXISTS `users` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`facebookId` VARCHAR(128) NOT NULL UNIQUE,
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `lists` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `permissions` (
-	`userId` INT NOT NULL,
-	`listId` INT NOT NULL,
-	PRIMARY KEY (`userId`, `listId`),
-	FOREIGN KEY (`userId`) REFERENCES `users`(`id`),
-	FOREIGN KEY (`listId`) REFERENCES `lists`(`id`)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `items` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`listId` INT NOT NULL,
-	`name` TEXT NOT NULL,
-	`date` DATETIME NOT NULL,
-	`value` DECIMAL(10,2) NOT NULL,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`listId`) REFERENCES `lists`(`id`)
-) ENGINE=InnoDB;
-
+# use contents of ./docker/mysql-init/init.sql.hidden
