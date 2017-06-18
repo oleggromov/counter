cf9cf2e1ea6001dcde0173acdcc9c332b09fc3f8
Oleg Gromov <mail@oleggromov.com>
2017-06-18 14:45:29 -0700

    Removes postfixes dev and demo from image names

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,7 @@
 source "./secrets/docker-repository.sh"
 read -p 'Tag: ' TAG
 
-docker build -t ${COUNTER_DOCKER_REPOSITORY}/counter-app:${TAG}-demo --build-arg NODE_ENV=production .
-# docker build -t ${COUNTER_DOCKER_REPOSITORY}/counter-app:${TAG}-dev --build-arg NODE_ENV=development .
+docker build -t ${COUNTER_DOCKER_REPOSITORY}/counter-app:${TAG} --build-arg NODE_ENV=production .
+# docker build -t ${COUNTER_DOCKER_REPOSITORY}/counter-app:${TAG} --build-arg NODE_ENV=development .
 
 unset COUNTER_DOCKER_REPOSITORY
